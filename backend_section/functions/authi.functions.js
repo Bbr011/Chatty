ec69b7c3062f85ffdd05209e1181ae901776bf94
Bbr011 <khadraouih7@gmail.com>
2025-01-06 21:31:55 +0100

    login/out and register handling

--- backend_section/functions/authi.functions.js
+++ backend_section/functions/authi.functions.js
@@ -4,16 +4,43 @@ import generateTokens from "../tools/generatetokens.js";
 
 
 export const login = async(req, res) => {
-    res.send("login user");
+    try {
+        const {username, password} = req.body;                                //user login blueprint
+        const user = await Usermodel.findOne({username});                     //find user by username
+        const isMatch = await bcrypt.compare(password, user?.password || ""); //password validation
+
+        if (!user || !isMatch){
+            return res.status(400).json({message: "invalid inputs"});
+        }
+
+        generateTokens(user._id, res);
+
+        res.status(200).json({
+            _id: user._id,
+            name: user.name,
+            username: user.username,
+            profilepic: user.profilepic
+        });
+    } catch(error){
+        console.log("error trying to login", error.message);
+        res.status(500).json({message: "something went wrong"});
+    }
 }
 
-export const logout = async(req, res) => {
-    res.send("logout user");
+export const logout = (req, res) => {
+    try {
+        res.cookie("jwt", "", {maxAge: 0});
+        res.status(200).json({message: "logged out"});
+
+    }catch(error){
+        console.log("error trying to logout", error.message);
+        res.status(500).json({message: "something went wrong"});
+    }
 }
 
 export const register = async(req, res) => {
     try{
-        const {name, username, password, confirmpassword, sex} = req.body; //user blueprint
+        const {name, username, password, confirmpassword, sex} = req.body; //user register blueprint
 
         if (password !== confirmpassword){
             return res.status(400).json({message: "enter the same password"});
