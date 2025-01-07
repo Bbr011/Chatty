ba17ac36c1d48f98d95a077ac0d432a5585765cc
Bbr011 <khadraouih7@gmail.com>
2025-01-07 01:34:07 +0100

    handling some routes

--- backend_section/index.js
+++ backend_section/index.js
@@ -1,6 +1,9 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
 import authiroutes from './routes/authi.routes.js';
+import msgroutes from './routes/msg.routes.js';
+import userroutes from './routes/user.routes.js';
 import connection from './db/connection.js';
 
 
@@ -8,7 +11,9 @@ const PORT = process.env.PORT || 5000;
 const application = express();
 
 dotenv.config();
+
 application.use(express.json());
+application.use(cookieParser());
 
 application.listen(PORT, () => {
     connection();
@@ -21,3 +26,5 @@ application.listen(PORT, () => {
 
 
 application.use("/api/authi", authiroutes);
+application.use("/api/messages", msgroutes);
+application.use("/api/users", userroutes);
