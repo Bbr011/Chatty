0d398ea12835a54c74d3d0e4954404b5035c71de
Bbr011 <khadraouih7@gmail.com>
2025-01-09 00:04:48 +0100

    logout button functionality

--- frontend_section/src/components/sidebarcomps/logoutbtn.jsx
+++ frontend_section/src/components/sidebarcomps/logoutbtn.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import { CiLogout } from "react-icons/ci";
+import Getlogout from '../../plugins/getlogout';
 
 const LogoutButton = () => {
+  const {loading, logout} = Getlogout()
   return (
     <div className='mt-3'>
-        <button type='submit' className='btn w-full border-green-400 hover:border-green-500 rounded-xl bg-green-400 hover:bg-green-500 text-white'>
-                <CiLogout className='w-4 h-4 text-white cursor-pointer'/>
-        </button>
+        {!loading ? (
+          <button type='submit' className='btn w-full border-green-400 hover:border-green-500 rounded-xl bg-green-400 hover:bg-green-500 text-white' onClick={logout}>
+            <CiLogout className='w-4 h-4 text-white cursor-pointer'/>
+          </button>
+        ) : (
+          <span className='loading loading-spinner'></span>
+        )}
     </div>
             
   )
