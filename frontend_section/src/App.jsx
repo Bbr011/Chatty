6c2318249ff5c89cd0bee7c5d7fca04063cb1cce
Bbr011 <khadraouih7@gmail.com>
2025-01-09 00:11:58 +0100

    Tabs functionality and optimization

--- frontend_section/src/App.jsx
+++ frontend_section/src/App.jsx
@@ -1,11 +1,22 @@
 import './App.css'
 import Mainpage from './Tabs/main/mainpage.jsx';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Login from './Tabs/login/login.jsx';
+import Registry from './Tabs/registry/register.jsx';
+import { Toaster } from 'react-hot-toast';
+import { useAuth } from './context/authdirection.jsx';
 
 function App() {
+  const {auth} = useAuth()
 
   return (
     <div className='p-4 h-screen flex items-center justify-center'>
-      <Mainpage />
+      <Routes>
+        <Route path="/" element={auth ? <Mainpage /> : <Navigate to="/login"/>}/>
+        <Route path="/register" element={auth ? <Navigate to="/"/> : <Registry />}/>
+        <Route path="/login" element={auth ? <Navigate to="/" /> : <Login />}/>
+      </Routes>
+      <Toaster />
     </div>
   );
 }
