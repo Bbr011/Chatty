6e74a88d3b6b627fa9754d6e94ab06f1a741f685
Bbr011 <khadraouih7@gmail.com>
2025-01-24 00:55:55 +0100

    handel the selected conversation

--- frontend_section/src/components/sidebarcomps/user.jsx
+++ frontend_section/src/components/sidebarcomps/user.jsx
@@ -1,23 +1,31 @@
 import React from 'react'
+import ShowConvos from '../../zustand/showconvos'
 
-const User = () => {
+const User = ({Convo, lastbox}) => {
+    const {selectedconvo, setselectedconvo} = ShowConvos();
+
+    const selected = selectedconvo?._id === Convo._id;
   return (
     <>
-        <div className='flex items-center gap-3 p-2 hover:backdrop-blur-xl rounded py-2 cursor-pointer'>
+        <div className={`flex items-center gap-3 p-2 hover:backdrop-blur-xl rounded py-2 cursor-pointer
+            ${selected ? "bg-green-300 bg-opacity-40" : ""}`}
+            onClick={() => {console.log('Setting selected convo:', Convo);
+                setselectedconvo(Convo);}}
+        >
             <div className='avatar online'>
                 <div className='w-12 rounded-full'>
-                    <img src="https://randomuser.me/api/portraits" alt="user pfp"></img>
+                    <img src={Convo.profilepic} alt="user pfp"></img>
                 </div>
             </div>
 
             <div className='flex flex-col flex-1'>
                 <div className='flex gap-3 justify-between'>
-                    <p className='font-bold text-gray-200'>User Name</p>
+                    <p className='font-bold text-gray-200'>{Convo.name}</p>
                 </div>
             </div>
         </div>
 
-        <div className='divider my-0 py-0 h-[1px] bg-green-300' />
+        {!lastbox && <div className='divider my-0 py-0 h-[1px] bg-green-300' />}
     </>
   )
 }
