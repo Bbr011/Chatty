568d1b3c3b9ed3a2537c8cf952d8fbeea1e49a8c
Bbr011 <khadraouih7@gmail.com>
2025-01-24 00:57:09 +0100

    show users except the main user

--- frontend_section/src/components/sidebarcomps/userslist.jsx
+++ frontend_section/src/components/sidebarcomps/userslist.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
 import User from './User'
+import Getconvos from '../../plugins/getconvos.js'
 
 const Userlist = () => {
+  const {loading, convos} = Getconvos();
+  console.log(convos);
   return (
     <div className='py-2 flex flex-col overflow-auto'>
-        <User />
-        <User />
-        <User />
-        <User />
-        <User />
-        <User />
-        <User />
-        <User />
+        {convos.map((Convo, idx) => (
+            <User key={Convo._id}
+            Convo={Convo}
+            lastbox={idx === convos.length - 1}
+            />
+        ))}
+
+        {loading ? <span className='laoding loading-spinner mx-auto'></span> : null}
     </div>
   )
 }
