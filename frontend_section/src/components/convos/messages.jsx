90d37187c97b3e2aade163f7684f5a424855474e
Bbr011 <khadraouih7@gmail.com>
2025-01-26 01:53:36 +0100

    map the conversation messages

--- frontend_section/src/components/convos/messages.jsx
+++ frontend_section/src/components/convos/messages.jsx
@@ -1,18 +1,23 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import Message from './message'
+import Getmessages from '../../plugins/getmessages'
+import MessageSkeleton from '../skeletons/messageloading'
+
 
 const Messages = () => {
+  const {messages, loading} = Getmessages()
+  const lastmsg = useRef()
+  useEffect(() => {
+    setTimeout(() => {
+      lastmsg.current?.scrollIntoView({behavior: 'smooth'})
+    }, 100)
+  },[messages])
+
   return (
     <div className='px-4 flex-1 overflow-auto'>
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
+        {!loading && messages.length > 0 && messages.map((message) => (<div key={message._id} ref={lastmsg}><Message message={message}/></div>))}
+        {loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx}/>)}
+        {!loading && messages.length === 0 && <p className='text-center text-gray-200'>No messages yet between this user</p>}
     </div>
   )
 }
