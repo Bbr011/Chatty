7e63e88638d1f0d30e470648ec27dd9fa6296412
Bbr011 <khadraouih7@gmail.com>
2025-01-26 01:52:29 +0100

    message properties

--- frontend_section/src/components/convos/message.jsx
+++ frontend_section/src/components/convos/message.jsx
@@ -1,17 +1,28 @@
 import React from 'react'
+import { useAuth } from '../../context/authdirection'
+import ShowConvos from '../../zustand/showconvos'
+import { extractTime } from '../../utils/currenttime';
 
-const Message = () => {
+const Message = ({message}) => {
+  const { auth } = useAuth();
+  const {selectedconvo} = ShowConvos();
+  const isitmine = message.senderID === auth._id;
+  const chatstate = isitmine ? "chat-end" : "chat-start";
+  const profilepic = isitmine ? auth.profilepic : selectedconvo.profilepic;
+  const bubblecolor = isitmine ? 'bg-green-400' : 'bg-gray-400';
+  const currenttime = extractTime(message.createdAt);
+
   return (
-    <div className='chat chat-end'>
+    <div className={`chat ${chatstate}`}>
         <div className='chat-image avatar'>
             <div className='w-10 rounded-full'>
-                <img src="https://randomuser.me/api/portraits" alt="user pfp"></img>
+                <img src={profilepic} alt="user pfp"></img>
             </div>
         </div>
-        <div className={`chat-bubble bg-green-400 text-white`}>
-            hey lil bro
+      <div className={`chat-bubble text-white ${bubblecolor}`}>
+            {message.message}
         </div>
-        <div className='chat-footer text-xs gap-1 flex items-center text-green-100'>02:42</div>
+        <div className='chat-footer text-xs gap-1 flex items-center text-green-100'>{currenttime}</div>
     </div>
   )
 }
