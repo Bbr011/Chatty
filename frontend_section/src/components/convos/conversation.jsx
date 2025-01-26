20dfbaa1da25e57aff8323e65ccdc5540fb7af31
Bbr011 <khadraouih7@gmail.com>
2025-01-26 20:01:48 +0100

    conversation container

--- frontend_section/src/components/convos/conversation.jsx
+++ frontend_section/src/components/convos/conversation.jsx
@@ -2,6 +2,7 @@ import React,{ useEffect } from 'react'
 import Messages from './messages'
 import Messagesinput from './messagesinput'
 import ShowConvos from '../../zustand/showconvos'
+import { useAuth } from '../../context/authdirection'
 
 const Conversation = () => {
     const {selectedconvo, setselectedconvo} = ShowConvos();
@@ -28,10 +29,11 @@ export default Conversation
 
 
 const NoChatSelected = () => {
+    const {auth} = useAuth();
 	return (
 		<div className='flex items-center justify-center w-full h-full'>
 			<div className='px-4 text-center sm:text-lg md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2'>
-				<p>Welcome User Name, ready to use <span className='text-emerald-300 font-bold'>CHATTY</span>?</p>
+				<p>Welcome {auth.name}, ready to use <span className='text-emerald-300 font-bold'>CHATTY</span>?</p>
 				<p>Check your new messages</p>
 			</div>
 		</div>
