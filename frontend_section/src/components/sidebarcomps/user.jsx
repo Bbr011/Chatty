acb0fd6c348e79924638927d54e796db1fc8c5f1
Bbr011 <khadraouih7@gmail.com>
2025-01-26 20:03:10 +0100

    select conversation

--- frontend_section/src/components/sidebarcomps/user.jsx
+++ frontend_section/src/components/sidebarcomps/user.jsx
@@ -1,33 +1,37 @@
 import React from 'react'
 import ShowConvos from '../../zustand/showconvos'
+import { useSocket } from '../../context/socketcontext'
 
 const User = ({Convo, lastbox}) => {
     const {selectedconvo, setselectedconvo} = ShowConvos();
 
     const selected = selectedconvo?._id === Convo._id;
-  return (
-    <>
-        <div className={`flex items-center gap-3 p-2 hover:backdrop-blur-xl rounded py-2 cursor-pointer
-            ${selected ? "bg-green-300 bg-opacity-40" : ""}`}
-            onClick={() => {console.log('Setting selected convo:', Convo);
-                setselectedconvo(Convo);}}
-        >
-            <div className='avatar online'>
-                <div className='w-12 rounded-full'>
-                    <img src={Convo.profilepic} alt="user pfp"></img>
+    const {onlineUsers} = useSocket();
+    const isonline = onlineUsers.includes(Convo._id);
+
+    return (
+        <>
+            <div className={`flex items-center gap-3 p-2 hover:backdrop-blur-xl rounded py-2 cursor-pointer
+                ${selected ? "bg-green-300 bg-opacity-40" : ""}`}
+                onClick={() => {console.log('Setting selected convo:', Convo);
+                    setselectedconvo(Convo);}}
+            >
+                <div className={`avatar ${isonline ? "online" : ""}`}>
+                    <div className='w-12 rounded-full'>
+                        <img src={Convo.profilepic} alt="user pfp"></img>
+                    </div>
+                </div>
+
+                <div className='flex flex-col flex-1'>
+                    <div className='flex gap-3 justify-between'>
+                        <p className='font-bold text-gray-200'>{Convo.username}</p>
+                    </div>
                 </div>
             </div>
 
-            <div className='flex flex-col flex-1'>
-                <div className='flex gap-3 justify-between'>
-                    <p className='font-bold text-gray-200'>{Convo.username}</p>
-                </div>
-            </div>
-        </div>
-
-        {!lastbox && <div className='divider my-0 py-0 h-[1px] bg-green-300' />}
-    </>
-  )
+            {!lastbox && <div className='divider my-0 py-0 h-[1px] bg-green-300' />}
+        </>
+    )
 }
 
 export default User
